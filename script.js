2ae76e60a2ad1a1a107f2c27154db72e4ba45a85
hisong984-jpg <hisong984@gmail.com>
2026-02-12 21:52:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function validateConfig() {
     const warnings = [];
 
     // Check required fields
-    if config.valentineName {
         warnings.push("Valentine's name is not set! Using default.");
         config.valentineName = "My Chrushiecakes";
-    }
+    
 
     // Validate colors
     const isValidHex = (hex) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex);
